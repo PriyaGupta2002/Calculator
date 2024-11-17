644ddbd7030106b5d890b26908aa30cda7988a4d
PriyaGupta2002 <priya200211gupta@gmail.com>
2024-11-17 16:53:29 +0000

    Index.JS

---
+++ index.js
@@ -0,0 +1,58 @@
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    document.getElementById('display').value = currentInput;
+}
+
+function setOperation(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculateResult();
+    }
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operator = '';
+    document.getElementById('display').value = '';
+}
+
+function calculateResult() {
+    if (currentInput === '' || previousInput === '') return;
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+
+    switch (operator) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            if (current === 0) {
+                result = 'Error';
+            } else {
+                result = prev / current;
+            }
+            break;
+        default:
+            return;
+    }
+
+    document.getElementById('display').value = result;
+    currentInput = result.toString();
+    operator = '';
+    previousInput = '';
+}
